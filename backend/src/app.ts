890e808782cdc45c1ee03ce0ce6b449da5e5aafb
RosarioB <borgesiros@gmail.com>
2025-06-24 15:31:16 +0200

    Removed duplicate lines form app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,6 @@ const app = express();
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.json());
 app.use("/", indexRoute);
 
 const port = process.env.PORT || 3001;
